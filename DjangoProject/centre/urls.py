99264f752009cfa5af00fe11af6ebeffcbf18ced
Pol Hidalgo <polhidalgolle7@gmail.com>
2025-03-13 18:19:20 +0100

    Delete DjangoProject directory

--- DjangoProject/centre/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('students', views.students_view, name='students'),
-
-    path('teachers', views.teachers_view, name='teachers')
-]
